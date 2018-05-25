41fce49a00920cb600f057acad71e90029412bfe
ERFFFFF <dakevcraft@yahoo.fr>
2018-05-25 11:06:42 +0200

    first

---
+++ src/main/java/fr/exia/ws7/jpu2016_hero.java
@@ -0,0 +1,8 @@
+package fr.exia.ws7;
+
+
+public abstract class jpu2016_hero {
+
+	public static void main(final String[] args) {
+	}
+}
---
+++ src/test/java/fr/exia/ws7/HitTest.java
@@ -0,0 +1,29 @@
+package fr.exia.ws7;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HitTest {
+
+	@Test
+	public void testHit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetStrength() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRange() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTypeHit() {
+		fail("Not yet implemented");
+	}
+
+}
